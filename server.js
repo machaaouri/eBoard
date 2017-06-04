5ed1ff9a59f92bf0de183c14525a71671ccd6f78
Houssame Machaaouri <hmachaao@gmail.com>
2017-06-05 01:40:21 +0200

    create a controller

--- server.js
+++ server.js
@@ -2,8 +2,13 @@ var http = require('http');
 var express = require("express");
 var app = express();
 
+var controllers = require('./controllers');
+
 
 app.set("view engine","vash");
+
+//Map the routes
+controllers.init(app);
 
 app.get("/",function(req,res){
 
